a9b22d343ef5f2f1f773d57071ea35e8fb48fc3d
Stefan Kert <stefan.kert@inode.at>
2015-11-30 12:10:42 +0100

    Changed lots of things

--- src/CodeExamples/CodeExamples/Program.cs
+++ src/CodeExamples/CodeExamples/Program.cs
@@ -12,27 +12,13 @@ namespace CodeExamples
         {
         }
 
+        public void test() {
 
-        public static bool IsPrime(int candidate) {
-            // Test whether the parameter is a prime number.
-            if ((candidate & 1) == 0) {
-                if (candidate == 2) {
-                    return true;
-                }
-                else {
-                    return false;
-                }
-            }
-            // Note:
-            // ... This version was changed to test the square.
-            // ... Original version tested against the square root.
-            // ... Also we exclude 1 at the end.
-            for (int i = 3; (i*i) <= candidate; i += 2) {
-                if ((candidate%i) == 0) {
-                    return false;
-                }
-            }
-            return candidate != 1;
+
+            int d = 10;
+
+            int numberOfDaysUntildelivery = 10;
         }
+
     }
 }
